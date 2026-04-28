660f5a46315f90f87045c419908edd4d79bf685d
amartinezplm <amartinez@plm.com.co>
2026-04-28 12:11:34 -0500

    feat: agregar appiId para despliegue en sanity studio, sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'z0kjs7n0ovpwlv8zypushhoz',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
